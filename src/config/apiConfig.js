ef70bee626be2bdee6f0b59401778ab2dd21485e
notifyhr2026-afk <notifyhr2026@gmail.com>
2026-01-01 21:52:59 +0530

    config changes

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,16 +1,16 @@
-const apiConfig = {
-    IdentityURL: 'http://192.168.1.10:36522/api',
-    HRserviceURL: 'http://192.168.1.10:36523/api',
-    AttendanceserviceURL: 'http://192.168.1.10:36524/api',
-    RecruitmentserviceURL: 'http://192.168.1.10:36525/api'
-};
-
 // const apiConfig = {
-//     baseURL: 'http://115.98.2.37:36522/api',
-//     HRserviceURL: 'http://115.98.2.37:36523/api',
-//     AttendanceserviceURL: 'http://115.98.2.37:36524/api',
-//     RecruitmentserviceURL: 'http://115.98.2.37:36525/api'
+//     IdentityURL: 'http://192.168.1.10:36522/api',
+//     HRserviceURL: 'http://192.168.1.10:36523/api',
+//     AttendanceserviceURL: 'http://192.168.1.10:36524/api',
+//     RecruitmentserviceURL: 'http://192.168.1.10:36525/api'
 // };
+
+const apiConfig = {
+    baseURL: 'http://115.98.2.37:36522/api',
+    HRserviceURL: 'http://115.98.2.37:36523/api',
+    AttendanceserviceURL: 'http://115.98.2.37:36524/api',
+    RecruitmentserviceURL: 'http://115.98.2.37:36525/api'
+};
 
 // const apiConfig = {
 //     baseURL: 'http://localhost:36522/api',
